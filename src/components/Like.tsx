5b19b6d04ac1b9ab381ed8f285469b102f119ac6
Multyjog <simon.kotov.web@gmail.com>
2024-01-18 10:19:27 +0700

    - State managing solutions tested - Reactivity test - Immer added

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,19 +1,27 @@
-import { CiHeart } from "react-icons/ci";
+import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { useState } from "react";
 
-const Like = () => {
+interface IProps {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: IProps) => {
   const [status, setStatus] = useState(false);
+  const toggleStatus = () => {
+    setStatus(!status);
+    onClick;
+  };
 
   if (!status)
     return (
       <div>
-        <CiHeart size="80"></CiHeart>
+        <FaRegHeart size="80" onClick={toggleStatus}></FaRegHeart>
       </div>
     );
   return (
     <div>
-      <FaHeart size={80} color="#e31b23"></FaHeart>
+      <FaHeart size="80" color="#e31b23" onClick={toggleStatus}></FaHeart>
     </div>
   );
 };
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,9 +1,10 @@
-function Message(){
-    const name = "Jog"
-    if(name)
-        return <h1>Hello {name}</h1>
-    return <h1>Hello World</h1>
+let counter = 0;
 
-}
+const Message = () => {
+  console.log("Called", counter);
 
-export default Message
+  counter++;
+  return <div>Message {counter}</div>;
+};
+
+export default Message;
